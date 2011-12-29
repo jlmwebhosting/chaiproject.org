b229a3901ba5568a639860be8579029c896a71d9
Rushabh Mehta <rmehta@gmail.com>
2011-12-29 14:52:54 +0530

    added all.css and all.js

---
+++ __init__.py
@@ -0,0 +1,16 @@
+concat = {
+	'all.js': [
+		'lib/js/json2.js',
+		'lib/js/jquery/jquery.min.js',
+		'lib/js/history.min.js',
+		'lib/views/misc.js',
+		'lib/js/core.js',
+		'lib/views/toolbar.js',
+		'chaiproject.org/views/app.js'
+	],
+	'all.css': [
+		'lib/css/bootstrap.css',
+		'lib/css/style.css',
+		'chaiproject.org/css/style.css',
+	]
+}
