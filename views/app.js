40f1001227c0f71e0f8885adbeb7c9a13ef02a94
Rushabh Mehta <rmehta@gmail.com>
2011-12-29 12:25:18 +0530

    fixes to style

--- views/app.js
+++ views/app.js
@@ -1,6 +1,8 @@
-app.version = -1 // dev mode
+app.version = -1; // dev mode
 app.cms = true;
-app.title = 'chaiproject.org';
+app.brand = '<img src="chaiproject.org/files/chai21x30.png"\
+	style="margin-right: 4x; margin-bottom: -8px; margin-top: -6px">\
+	<span>chaiproject.org</span>';
 
 $(document).ready(function() {
 });
@@ -9,4 +11,5 @@ $(document).bind('_show', function() {
   $.require('lib/js/google-code-prettify/prettify.css');
   $.require('lib/js/google-code-prettify/prettify.js');
   prettyPrint();
+
 })
